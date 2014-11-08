5e38433d955adce7f6e7f51d5688b97b676fb2de
Chris Denniston <octopuscabbage@gmail.com>
2014-11-08 17:01:06 -0600

    added cardinal tostring function

--- src/Cardinal.h
+++ src/Cardinal.h
@@ -1,7 +1,22 @@
 #ifndef CARDINAL_H
 #define CARDINAL_H
+
+#include <string>
+
 enum Cardinal { NORTH,
 		SOUTH,
 		EAST,
 		WEST};
+string cardinalToString(Cardinal c){
+	swtich(c){
+		case NORTH:
+			return "NORTH";
+		case SOUTH:
+			return "SOUTH";
+		case WEST:
+			return "WEST";
+		case EAST:
+			return "EAST";
+	}
+}
 #endif
