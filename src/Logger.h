cec10cbc39e68f6246ac93472dc05d903cf6f892
Chris Denniston <octopuscabbage@gmail.com>
2014-11-14 19:41:07 -0600

    finished adding documentation'

--- src/Logger.h
+++ src/Logger.h
@@ -7,6 +7,32 @@
 #include <sstream>
 using namespace std;
 
+/**
+ * Singleton logger class
+ *
+ * Defaults to cout and prepending "Message: " to messages and 
+ * "Error: " to errors
+ *
+ * Usage:
+ *
+ * //Initialies the stream to cout, the message prefix to "Message: "
+ * //and the error prefix to "Error: "
+ * Logger::standardInit(); 
+ *
+ * //Logs "Message: This is a message" to standard output
+ * Logger::logMessage("This is a message");
+ * 
+ * //Logs "Error: This is an error" to standard output
+ * Logger::logError("This is an error");
+ *
+ * //Set the logger to a file stream
+ * fstream fout("/tmp/log.txt");
+ * Logger::setStream(&fout);
+ *
+ * //Set the Message Prefixes
+ * Logger::setMessagePrefix("M: ");
+ * Logger::setErrorPrefix("E: ");
+ */
 class Logger{
 private:
 	ostream* os;
@@ -29,11 +55,29 @@ public:
 		return instance;
 	}
 
+	/*
+	 * Sets the stream object to stream to, must be passed as a pointer
+	 */
 	static void setStream(ostream* out);
+	/*
+	 * Logs a message to the set stream with the standard message prefix
+	 */
 	static void logMessage(string message);
+	/*
+	 * Logs an error to the set stream with the standard message prefix
+	 */
 	static void logError(string error);
+	/*
+	 * Logs a message to the set stream with the standard error prefix
+	 */
 	static void setMessagePrefix(string s);
+	/*
+	 * Sets the prefix for all error messages
+	 */
 	static void setErrorPrefix(string s);
+	/*
+	 * Sets teh prefix for all messages
+	 */
 	static void standardInit();
 };
 	
